342901cf28e5b143d10ec2f89634edc675651fd0
David Kung <dtw.kung@gmail.com>
2019-12-14 17:33:44 +1100

    first commit

---
+++ webServer.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, Response
+from flask_basicauth import BasicAuth
+from moveDetect import moveDetect
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'dtwkung'
+app.config['BASIC_AUTH_PASSWORD'] = 'testtest'
+app.config['BASIC_AUTH_FORCE'] = True
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+@basic_auth.required
+def index():
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.getFrame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(moveDetect(model='mobilenet')),
+                    mimetype = 'multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug = True, ssl_context='adhoc', port=53189)
